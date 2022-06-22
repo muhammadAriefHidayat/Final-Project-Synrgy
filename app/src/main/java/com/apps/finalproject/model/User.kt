bc8a9ac1016c6568e8232b6f02333ab19bf48419
Sitifatimah121 <63435630+Sitifatimah121@users.noreply.github.com>
2022-06-22 10:03:22 +0700

    Revert "Revert 34 features/detail produk"

--- app/src/main/java/com/apps/finalproject/model/User.kt
+++ app/src/main/java/com/apps/finalproject/model/User.kt
@@ -5,8 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class User(
     @SerializedName("name")
     var name: String,
-    @SerializedName("phone")
-    var phone: String?,
     @SerializedName("skinType")
     var skinType: String?,
 )
