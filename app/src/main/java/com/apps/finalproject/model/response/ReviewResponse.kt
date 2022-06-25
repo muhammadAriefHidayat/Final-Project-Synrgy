f9c59a4c19021c06154776658b43d3bde8573a06
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-25 21:17:21 +0700

    add review response

---
+++ app/src/main/java/com/apps/finalproject/model/response/ReviewResponse.kt
@@ -0,0 +1,57 @@
+package com.apps.finalproject.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ReviewResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItem>,
+
+	@field:SerializedName("success")
+	val success: Boolean,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class DataItem(
+
+	@field:SerializedName("date")
+	val date: String,
+
+	@field:SerializedName("averageStar")
+	val averageStar: Int,
+
+	@field:SerializedName("effective")
+	val effective: Int,
+
+	@field:SerializedName("images")
+	val images: List<String>,
+
+	@field:SerializedName("productId")
+	val productId: String,
+
+	@field:SerializedName("price")
+	val price: Int,
+
+	@field:SerializedName("texture")
+	val texture: Int,
+
+	@field:SerializedName("imagesCount")
+	val imagesCount: Int,
+
+	@field:SerializedName("packaging")
+	val packaging: Int,
+
+	@field:SerializedName("id")
+	val id: String,
+
+	@field:SerializedName("userId")
+	val userId: String,
+
+	@field:SerializedName("content")
+	val content: String
+)
