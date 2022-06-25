32d846f5d6cb0c645c770ade8e34e3ab78aa561d
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-25 21:55:51 +0700

    update services

--- app/src/main/java/com/apps/finalproject/data/api/ApiServices.kt
+++ app/src/main/java/com/apps/finalproject/data/api/ApiServices.kt
@@ -4,7 +4,9 @@ import com.apps.finalproject.model.LoginBody
 import com.apps.finalproject.model.RegisterBody
 import com.apps.finalproject.model.response.LoginResponse
 import com.apps.finalproject.model.response.RegisterResponse
+import com.apps.finalproject.model.response.ReviewResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiServices {
@@ -17,4 +19,8 @@ interface ApiServices {
     suspend fun register(
         @Body register: RegisterBody
     ): RegisterResponse
+
+    @GET("/api/v1/review")
+    suspend fun review(
+    ): ReviewResponse
 }
