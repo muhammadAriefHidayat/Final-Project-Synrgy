66f91b72d5121ceeb91bc6b3f4ca5eda474eb4d4
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-25 19:27:57 +0700

    add product response

---
+++ app/src/main/java/com/apps/finalproject/model/response/ProductResponse.kt
@@ -0,0 +1,69 @@
+package com.apps.finalproject.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItem>,
+
+	@field:SerializedName("success")
+	val success: Boolean,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class DataItem(
+
+	@field:SerializedName("quantity")
+	val quantity: Int,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("variant")
+	val variant: List<VariantItem>,
+
+	@field:SerializedName("id")
+	val id: String,
+
+	@field:SerializedName("brand")
+	val brand: Brand
+)
+
+data class Brand(
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("banner")
+	val banner: String,
+
+	@field:SerializedName("description")
+	val description: String,
+
+	@field:SerializedName("logo")
+	val logo: String,
+
+	@field:SerializedName("id")
+	val id: String
+)
+
+data class VariantItem(
+
+	@field:SerializedName("quantity")
+	val quantity: Int,
+
+	@field:SerializedName("price")
+	val price: Int,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("id")
+	val id: String
+)
