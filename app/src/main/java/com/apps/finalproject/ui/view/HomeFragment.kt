76e7803de7dee9194c1445d5d09c985108e0a0f1
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-29 10:35:11 +0700

    implement home and update layout

--- app/src/main/java/com/apps/finalproject/ui/view/HomeFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/view/HomeFragment.kt
@@ -1,15 +1,25 @@
 package com.apps.finalproject.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.apps.finalproject.R
+import com.apps.finalproject.databinding.FragmentHomeBinding
+import com.apps.finalproject.model.Article
+import com.apps.finalproject.ui.ViewModelFactory
+import com.apps.finalproject.ui.adapter.ListArticleAdapter
+import com.apps.finalproject.ui.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private lateinit var binding: FragmentHomeBinding
+    private val homeViewModel: HomeViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
     }
 
     override fun onCreateView(
@@ -17,6 +27,26 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("getArticle", "onViewCreated: data article")
+
+        homeViewModel.getArticle()
+        homeViewModel.article.observe(viewLifecycleOwner){
+            showDataArticle(it)
+        }
+    }
+
+    private fun showDataArticle(listArticle: List<Article>) {
+        val listArticleAdapter = ListArticleAdapter(listArticle)
+        binding.itemListArticle.apply {
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = listArticleAdapter
+        }
     }
 }
