1536272cc7496c4be21d20d7ffc466d8b9ff33d8
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-06 17:00:17 +0700

    profil category item

--- app/src/main/java/com/apps/finalproject/ui/view/ProfileActivity.kt
+++ app/src/main/java/com/apps/finalproject/ui/view/ProfileActivity.kt
@@ -3,11 +3,15 @@ package com.apps.finalproject.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.apps.finalproject.databinding.ActivityProfileBinding
+import com.apps.finalproject.model.ProfilCategoryItem
+import com.apps.finalproject.model.ProfilItem
+import com.apps.finalproject.ui.adapter.ProfilAdapter
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 
 class ProfileActivity : AppCompatActivity() {
-    lateinit var  binding : ActivityProfileBinding
+    private lateinit var itemArrayList: ArrayList<ProfilItem>
+    lateinit var binding: ActivityProfileBinding
     private val adapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,14 @@ class ProfileActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        itemArrayList = ProfilCategoryItem.profilItemList
+        itemArrayList.forEach {
+            adapter.add(ProfilAdapter(it, this))
+        }
+
+        binding.apply {
+            rvProfil.adapter = adapter
+        }
     }
 }
