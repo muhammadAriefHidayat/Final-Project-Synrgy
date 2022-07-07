16b0c9ce9b981775bdb40d56e70a74f5a6331ec2
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-07 22:31:53 +0700

    update detail view model

--- app/src/main/java/com/apps/finalproject/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/apps/finalproject/ui/detail/DetailViewModel.kt
@@ -6,23 +6,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apps.finalproject.data.repository.MainRepository
+import com.apps.finalproject.remote.model.Brand
 import com.apps.finalproject.remote.model.Review
+import com.apps.finalproject.remote.model.Trending
 import kotlinx.coroutines.launch
 
 class DetailViewModel (private val repository: MainRepository): ViewModel(){
     private var _review = MutableLiveData<List<Review>>()
     val review : LiveData<List<Review>> = _review
 
-//    private val _isLoading = MutableLiveData<Boolean>()
-//    val isLoading: LiveData<Boolean> = _isLoading
-
     fun getReview() = viewModelScope.launch {
         repository.getReview().collect{
-
-//            val mReview = it.data.forEach { review ->
-                _review.value = it
+             _review.value = it
                 Log.d("getReview", review.value.toString())
-//            }
         }
     }
 }
