3cddb212cb0c1f1bbb9042b903a17026bce58822
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-07 22:29:49 +0700

    add click listener in trending adapter

--- app/src/main/java/com/apps/finalproject/ui/adapter/ListTrendingAdapter.kt
+++ app/src/main/java/com/apps/finalproject/ui/adapter/ListTrendingAdapter.kt
@@ -1,8 +1,11 @@
 package com.apps.finalproject.ui.adapter
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import com.apps.finalproject.R
 import com.apps.finalproject.databinding.ItemListProdukTrendingBinding
@@ -15,7 +18,11 @@ class ListTrendingAdapter(private val listTrending: List<Trending>) : RecyclerVi
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     private lateinit var binding: ItemListProdukTrendingBinding
+    private lateinit var onItemClickListener: OnItemClickListener
 
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+        this.onItemClickListener = onItemClickListener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding = ItemListProdukTrendingBinding.inflate(
@@ -28,16 +35,24 @@ class ListTrendingAdapter(private val listTrending: List<Trending>) : RecyclerVi
         val productTrending = listTrending[position]
         with(binding){
             Glide.with(binding.root.context)
-                .load(productTrending.images[0])
+                .load(productTrending?.images?.get(0))
                 .centerCrop()
                 .placeholder(R.drawable.article_herbal)
                 .into(imageView2)
-            textView2.text = productTrending.brand.name
-            tvNameProduk.text = productTrending.variant.toListVariant()[0].name
-            textView3.text = productTrending.variant.toListVariant()[0].price.toString()
-            ratingBar2.rating = productTrending.average.toFloat()
+            textView2.text = productTrending.brand?.name ?: ""
+            tvNameProduk.text = productTrending?.variant?.get(0)?.name ?: ""
+            textView3.text = productTrending?.variant?.get(0)?.price.toString()
+            ratingBar2.rating = productTrending.average?.toFloat()!!
+            root.setOnClickListener{
+                Log.d(TAG, "onBindViewHolder: ${productTrending.variant?.get(0)?.name}")
+                onItemClickListener.onItemClicked(productTrending)
+            }
         }
     }
 
     override fun getItemCount(): Int = listTrending.size
+
+    interface OnItemClickListener{
+        fun onItemClicked(data: Trending)
+    }
 }
