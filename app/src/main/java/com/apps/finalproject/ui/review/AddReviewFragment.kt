a1c9c067fe1f2ad5b908dfe159ad4fd3a9d4d8b6
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-09 15:18:48 +0700

    add library image cropper

--- app/src/main/java/com/apps/finalproject/ui/review/AddReviewFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/review/AddReviewFragment.kt
@@ -1,7 +1,10 @@
 package com.apps.finalproject.ui.review
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +13,7 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import com.apps.finalproject.R
 import com.apps.finalproject.databinding.FragmentAddReviewBinding
+import com.bumptech.glide.Glide
 
 class AddReviewFragment : Fragment() {
 
@@ -35,6 +39,7 @@ class AddReviewFragment : Fragment() {
 
                 }
             }
+
             addPhoto.setOnClickListener {
                 val intentGalery = Intent(Intent.ACTION_PICK)
                 intentGalery.type = "image/*"
@@ -48,4 +53,43 @@ class AddReviewFragment : Fragment() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = data!!.data
+//
+//            CropImage.activity(imageUri)
+//                .setAspectRatio(1, 1)
+//                .start(this)
+        }
+
+//        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+//            val result = CropImage.getActivityResult(data)
+//
+//            if (resultCode == Activity.RESULT_OK) {
+//                progress.visibility = View.VISIBLE
+//
+//                val resultUri = result.uri
+//                val nama = nama_cluster.text.toString()
+//
+//                val filePath = mStorageRef!!.child("perumahan_profil_images")
+//                    .child("$nama.jpg")
+//
+//                /**
+//                 * mengupload file berupa URI image dan memanggil fungsi savetofirebase
+//                 * */
+//                filePath.putFile(resultUri).addOnSuccessListener {
+//                    filePath.downloadUrl.addOnSuccessListener {
+//                        Log.d("SettingActivity", "$it")
+//                        foto = it.toString()
+//                        Glide.with(this)
+//                            .load(it)
+//                            .placeholder(R.drawable.ic_home_work_24)
+//                            .into(cluster_foto);
+//                        progress.visibility = View.INVISIBLE
+//                    }
+//                }
+//            }
+//        }
+    }
 }
