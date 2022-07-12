b818039a03feeffe27232132ab6b3701d7d21c98
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-12 21:51:18 +0700

    create list cart response

---
+++ app/src/main/java/com/apps/finalproject/remote/response/GetCartResponse.kt
@@ -0,0 +1,35 @@
+package com.apps.finalproject.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GetCartResponse(
+
+    @field:SerializedName("overview")
+    val data: Overview,
+
+    @field:SerializedName("items")
+    val items: String,
+
+    @field:SerializedName("message")
+    val quantity: String,
+
+    @field:SerializedName("status")
+    val status: String
+)
+
+data class GetCartResult(
+    @field:SerializedName("id")
+    val id: String
+)
+data class Overview(
+    @field:SerializedName("id")
+    val id: String,
+    @field:SerializedName("promo")
+    val promo: String,
+    @field:SerializedName("delivery")
+    val delivery: String,
+    @field:SerializedName("total")
+    val total: String,
+    @field:SerializedName("weight")
+    val weight: String
+)
--- app/src/main/java/com/apps/finalproject/ui/cart/CartFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/cart/CartFragment.kt
@@ -1,20 +1,29 @@
 package com.apps.finalproject.ui.cart
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.apps.finalproject.R
+import androidx.fragment.app.Fragment
+import com.apps.finalproject.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
 
+    private lateinit var binding: FragmentCartBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
     }
 
 }
