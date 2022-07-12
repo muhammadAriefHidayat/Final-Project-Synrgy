c93395d5225126c5acbdfd757fa73b7464a7dc43
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-12 19:39:04 +0700

    cart vewimodel

--- app/src/main/java/com/apps/finalproject/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/apps/finalproject/ui/cart/CartViewModel.kt
@@ -1,26 +1,17 @@
 package com.apps.finalproject.ui.cart
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apps.finalproject.data.repository.MainRepository
-import com.apps.finalproject.remote.body.LoginBody
 import com.apps.finalproject.remote.model.Cart
-import com.apps.finalproject.remote.model.Token
 import com.apps.finalproject.remote.response.CartResponse
-import com.apps.finalproject.remote.response.LoginResponse
-import com.apps.finalproject.utils.AppPref
-import com.auth0.android.jwt.JWT
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class CartViewModel(private val repository: MainRepository) : ViewModel() {
-
-    private val listToken = MutableLiveData<Token>()
 
     fun AddCart(cart: Cart) = viewModelScope.launch {
         repository.addCart(cart).collect {
@@ -29,18 +20,15 @@ class CartViewModel(private val repository: MainRepository) : ViewModel() {
                     call: Call<CartResponse>,
                     response: Response<CartResponse>
                 ) {
-                    TODO("Not yet implemented")
+                    Log.d("hasil", response.body()?.success.toString())
                 }
 
                 override fun onFailure(call: Call<CartResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
+                    Log.d("hasil", "error")
                 }
 
             })
         }
     }
 
-    internal fun getToken(): LiveData<Token> {
-        return listToken
-    }
 }
