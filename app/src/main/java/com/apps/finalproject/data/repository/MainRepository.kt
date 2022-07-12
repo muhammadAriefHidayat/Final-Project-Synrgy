6016a1754d143f7cdf670337249530f2cb6c5f1e
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-12 18:57:02 +0700

    create cart MainRepository.kt

--- app/src/main/java/com/apps/finalproject/data/repository/MainRepository.kt
+++ app/src/main/java/com/apps/finalproject/data/repository/MainRepository.kt
@@ -3,12 +3,14 @@ package com.apps.finalproject.data.repository
 import com.apps.finalproject.data.datastore.RemoteDataSource
 import com.apps.finalproject.remote.body.LoginBody
 import com.apps.finalproject.remote.body.RegisterBody
+import com.apps.finalproject.remote.model.Cart
 
 class MainRepository (private val remoteDataSource: RemoteDataSource){
     fun register(registerBody: RegisterBody) = remoteDataSource.register(registerBody)
     fun login(loginBody: LoginBody) = remoteDataSource.login(loginBody)
     fun getReview() = remoteDataSource.getReview()
     fun getArticle() = remoteDataSource.getArticle()
+    fun addCart(cart: Cart) = remoteDataSource.addCart(cart)
     fun getProductTrending() = remoteDataSource.getProductTrending()
     fun getDetailTrending(productId: String) = remoteDataSource.getDetailTrending(productId)
 }
