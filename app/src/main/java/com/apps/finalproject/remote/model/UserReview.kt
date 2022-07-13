14d8e6735f9bd058dea1fe3506c33dfceb84af79
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-13 14:55:23 +0700

    add UserReview

--- app/src/main/java/com/apps/finalproject/remote/model/UserReview.kt
+++ app/src/main/java/com/apps/finalproject/remote/model/UserReview.kt
@@ -1,38 +1,27 @@
 package com.apps.finalproject.remote.model
 
 import android.os.Parcelable
+import com.apps.finalproject.remote.response.ListTrendingResponse
+import com.apps.finalproject.remote.response.UserReviewResponse
 import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class UserReview(
-	val data: List<DataItem>,
-	val success: Boolean,
-	val message: String,
-	val status: String
-) : Parcelable
-
-@Parcelize
-data class User(
 	val skinType: String,
-	val address: Any,
 	val phone: String,
 	val name: String,
 	val id: String,
 	val avatar: String
 ) : Parcelable
 
-@Parcelize
-data class DataItem(
-	val date: String,
-	val effective: Int,
-	val averageStar: Int,
-	val images: List<String>,
-	val productId: String,
-	val texture: Int,
-	val price: Int,
-	val imagesCount: Int,
-	val packaging: Int,
-	val id: String,
-	val user: User,
-	val content: String
-) : Parcelable
+fun UserReviewResponse.toUser() : UserReview {
+	return UserReview(
+		skinType = this.skinType,
+		phone = this.phone,
+		name = this.name,
+		id = this.id,
+		avatar = this.avatar
+	)
+}
+
