36f6f5975ee1f4e3b125e7d6148526b45e78817f
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-18 03:35:48 +0700

    Checkout Shipping #create main repositoy ongkir

--- app/src/main/java/com/apps/finalproject/data/repository/MainRepository.kt
+++ app/src/main/java/com/apps/finalproject/data/repository/MainRepository.kt
@@ -2,16 +2,18 @@ package com.apps.finalproject.data.repository
 
 import com.apps.finalproject.data.datastore.RemoteDataSource
 import com.apps.finalproject.remote.body.LoginBody
+import com.apps.finalproject.remote.body.PengirimanBody
 import com.apps.finalproject.remote.body.RegisterBody
 import com.apps.finalproject.remote.model.Cart
 
 class MainRepository (private val remoteDataSource: RemoteDataSource){
     fun register(registerBody: RegisterBody) = remoteDataSource.register(registerBody)
     fun login(loginBody: LoginBody) = remoteDataSource.login(loginBody)
-    fun getReview() = remoteDataSource.getReview()
-    fun getArticle() = remoteDataSource.getArticle()
     fun addCart(cart: Cart) = remoteDataSource.addCart(cart)
     fun getCart() = remoteDataSource.getCart()
+    fun postOngkir(pengirimanBody:PengirimanBody) = remoteDataSource.postOngkir(pengirimanBody)
+    fun getReview() = remoteDataSource.getReview()
+    fun getArticle() = remoteDataSource.getArticle()
     fun getProductTrending() = remoteDataSource.getProductTrending()
     fun getDetailTrending(productId: String) = remoteDataSource.getDetailTrending(productId)
     fun searchProductByName(name: String) = remoteDataSource.searchProductByName(name)
