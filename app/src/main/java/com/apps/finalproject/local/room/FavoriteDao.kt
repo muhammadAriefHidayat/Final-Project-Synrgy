eada6fed56d66e56d305b793218daf08f9448cf5
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-18 18:00:29 +0700

    add favorite dao

--- app/src/main/java/com/apps/finalproject/local/room/FavoriteDao.kt
+++ app/src/main/java/com/apps/finalproject/local/room/FavoriteDao.kt
@@ -1,4 +1,20 @@
 package com.apps.finalproject.local.room
 
-class FavoriteDao {
+import androidx.room.*
+import com.apps.finalproject.local.entity.Favorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+    @Query("SELECT * FROM favorite_product ORDER BY product_name ASC ")
+    fun getFavorite(): Flow<List<Favorite>>
+
+    @Query("SELECT * FROM favorite_product WHERE product_name = :product_name LIMIT 1")
+    fun getMyFavorite(product_name: String): Flow<Favorite>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite_product: Favorite)
+
+    @Delete
+    suspend fun deleteFavorite(favorite_product: Favorite)
 }
