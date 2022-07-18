c6259e502d985eb61af10f052b785083b0bec411
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-18 18:51:36 +0700

    Checkout Shipping #create adapter kurir #create adapter pengiriman and profile

--- app/src/main/java/com/apps/finalproject/ui/adapter/KurirAdapter.kt
+++ app/src/main/java/com/apps/finalproject/ui/adapter/KurirAdapter.kt
@@ -4,29 +4,26 @@ import android.content.Context
 import android.widget.ImageView
 import android.widget.TextView
 import com.apps.finalproject.R
-import com.apps.finalproject.remote.model.ProfilItem
+import com.apps.finalproject.remote.model.EkspedisiItem
 import com.apps.finalproject.utils.Utils.getDrawableInt
 import com.bumptech.glide.Glide
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
-class ProfilAdapter(val profilItem: ProfilItem, val context: Context) : Item<GroupieViewHolder>() {
+class KurirAdapter(val itemKurir: EkspedisiItem, val context: Context) : Item<GroupieViewHolder>() {
 
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        val imge = viewHolder.itemView.findViewById<ImageView>(R.id.img_profil_item)
-        val idDrawable = getDrawableInt(context,profilItem.img)
+        val imge = viewHolder.itemView.findViewById<ImageView>(R.id.img_kurir_item)
+        val idDrawable = getDrawableInt(context, itemKurir.img)
         Glide.with(context)
             .load(idDrawable)
             .placeholder(R.drawable.home)
             .into(imge);
-        viewHolder.itemView.findViewById<TextView>(R.id.tv_email_profil).text =  profilItem.itemProfil
-        viewHolder.itemView.setOnClickListener {
-            context.startActivity(profilItem.intent)
-        }
+        viewHolder.itemView.findViewById<TextView>(R.id.tv_kurir_name).text = itemKurir.kurir
     }
 
     override fun getLayout(): Int {
-        return R.layout.item_list_profile
+        return R.layout.item_list_kurir
     }
 }
