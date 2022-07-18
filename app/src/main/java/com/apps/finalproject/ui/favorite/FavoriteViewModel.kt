d63e9b87a87c047e62af173aa19d0da6a8958abb
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-18 18:02:59 +0700

    add favorite viewmodel

--- app/src/main/java/com/apps/finalproject/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/apps/finalproject/ui/favorite/FavoriteViewModel.kt
@@ -1,4 +1,28 @@
 package com.apps.finalproject.ui.favorite
 
-class FavoriteViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apps.finalproject.data.repository.MainRepository
+import com.apps.finalproject.remote.model.FavoriteProduct
+import kotlinx.coroutines.launch
+
+class FavoriteViewModel (
+    private val mainRepository: MainRepository
+): ViewModel(){
+
+    private val _favorite = MutableLiveData<List<FavoriteProduct>>()
+    val favorite: LiveData<List<FavoriteProduct>> = _favorite
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    fun getFavorite() = viewModelScope.launch {
+        _isLoading.value = true
+        mainRepository.getAllFavorite().collect{
+            _favorite.value = it
+            _isLoading.value = false
+        }
+    }
 }
