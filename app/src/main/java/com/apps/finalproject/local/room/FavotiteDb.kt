58e4ada7553307673b193d0c8e29de3011324616
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-18 18:00:49 +0700

    add favorite db

--- app/src/main/java/com/apps/finalproject/local/room/FavotiteDb.kt
+++ app/src/main/java/com/apps/finalproject/local/room/FavotiteDb.kt
@@ -1,4 +1,24 @@
 package com.apps.finalproject.local.room
 
-class FavotiteDb {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.apps.finalproject.local.entity.Favorite
+
+@Database(entities = [Favorite::class], version = 1, exportSchema = false)
+abstract class FavoriteDb : RoomDatabase(){
+    abstract fun favoriteDao() : FavoriteDao
+
+    companion object {
+        @Volatile
+        private var instance: FavoriteDb? = null
+        fun getInstance(context: Context): FavoriteDb =
+            instance ?: synchronized(this){
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    FavoriteDb::class.java, "Favorite_Product.db"
+                ).fallbackToDestructiveMigration().build().apply { instance = this }
+            }
+    }
 }
