0d7685f76fcb89f394bf5118a356bf1bf8b10fe6
Muhammad Arief Hidayat <tubagusarief45@gmail.com>
2022-07-20 13:24:42 +0700

    Payment finishing #fixing payment finsihing

--- app/src/main/java/com/apps/finalproject/ui/checkoutshiping/FinishingPaymentActivity.kt
+++ app/src/main/java/com/apps/finalproject/ui/checkoutshiping/FinishingPaymentActivity.kt
@@ -1,18 +1,15 @@
 package com.apps.finalproject.ui.checkoutshiping
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.apps.finalproject.R
+import androidx.appcompat.app.AppCompatActivity
 import com.apps.finalproject.databinding.ActivityFinishingPaymentBinding
 
 class FinishingPaymentActivity : AppCompatActivity() {
-    private  lateinit var binding:ActivityFinishingPaymentBinding
+    private lateinit var binding: ActivityFinishingPaymentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFinishingPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
     }
 }
