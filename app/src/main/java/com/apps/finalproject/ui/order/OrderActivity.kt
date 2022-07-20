c2db0a46955dc6a8efe75349d1cc59a7c101213d
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-20 20:53:10 +0700

    Orders #Implementasi OrdersViewModel.kt on Activity Order

--- app/src/main/java/com/apps/finalproject/ui/order/OrderActivity.kt
+++ app/src/main/java/com/apps/finalproject/ui/order/OrderActivity.kt
@@ -2,11 +2,31 @@ package com.apps.finalproject.ui.order
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.apps.finalproject.R
+import android.util.Log
+import androidx.activity.viewModels
+import com.apps.finalproject.databinding.ActivityOrderBinding
+import com.apps.finalproject.remote.response.DataOrders
+import com.apps.finalproject.ui.ViewModelFactory
 
 class OrderActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityOrderBinding
+    private val ordersViewModel:OrdersViewModel by viewModels {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        binding = ActivityOrderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ordersViewModel.getOrders()
+        ordersViewModel.getResponse().observe(this){
+            if (it.isNotEmpty()){
+                setData(it)
+            }
+        }
+    }
+
+    fun setData(list: List<DataOrders>) {
+        
     }
 }
