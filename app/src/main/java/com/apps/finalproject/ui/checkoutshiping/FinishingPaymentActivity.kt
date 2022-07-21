a3c27e208212e6303505f9db08bc3b70486f0083
Muhammad Arief Hidayat <tubagusarief45@gmail.com>
2022-07-22 02:13:08 +0700

    Payment # Format to Rupiah

--- app/src/main/java/com/apps/finalproject/ui/checkoutshiping/FinishingPaymentActivity.kt
+++ app/src/main/java/com/apps/finalproject/ui/checkoutshiping/FinishingPaymentActivity.kt
@@ -3,11 +3,12 @@ package com.apps.finalproject.ui.checkoutshiping
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ContentInfoCompat
 import com.apps.finalproject.databinding.ActivityFinishingPaymentBinding
 import com.apps.finalproject.remote.response.DataPayment
 import com.apps.finalproject.ui.order.OrderActivity
 import com.apps.finalproject.ui.view.HomePageActivity
+import com.apps.finalproject.utils.Utils
+import com.apps.finalproject.utils.Utils.rupiah
 
 class FinishingPaymentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFinishingPaymentBinding
@@ -19,22 +20,24 @@ class FinishingPaymentActivity : AppCompatActivity() {
 
         val intentPayment = intent.getParcelableExtra<DataPayment>("payment")
         binding.apply {
-            if (intentPayment != null){
+            if (intentPayment != null) {
                 tvNomorVirtual.text = intentPayment.vaNumber
-                tvTotalPembayaran.text = intentPayment.grossAmount.toString()
+                tvTotalPembayaran.text = rupiah(intentPayment.grossAmount.toDouble())
                 val orderid = (intentPayment.orderId).split("-")
                 tvIdPesanan.text = orderid[0].toString()
             }
         }
 
         binding.btnCekstatuspesanan.setOnClickListener {
-            val dashBoardIntent = Intent(this,OrderActivity::class.java)
-            dashBoardIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            val dashBoardIntent = Intent(this, OrderActivity::class.java)
+            dashBoardIntent.flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(dashBoardIntent)
         }
         binding.btnBelanjaLagi.setOnClickListener {
-            val dashBoardIntent = Intent(this,HomePageActivity::class.java)
-            dashBoardIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            val dashBoardIntent = Intent(this, HomePageActivity::class.java)
+            dashBoardIntent.flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(dashBoardIntent)
         }
     }
