8f9498e1e1d77265a74d7ba7a649221167aabb03
Stheven Erlangga Sanjaya <66986758+stheven26@users.noreply.github.com>
2022-07-23 15:34:16 +0700

    add new layout profile

---
+++ app/src/main/java/com/apps/finalproject/ui/view/AddressChoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.apps.finalproject.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddressChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_choice)
+    }
+}
