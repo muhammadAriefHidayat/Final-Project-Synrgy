e812d8a6101b1272cf463ae430573eb941f3de59
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-25 01:28:11 +0700

    add articleListFragment

--- app/src/main/java/com/apps/finalproject/ui/article/ArticleListFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/article/ArticleListFragment.kt
@@ -5,29 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.apps.finalproject.R
+import com.apps.finalproject.databinding.FragmentArticleListBinding
+import com.apps.finalproject.remote.model.Article
+import com.apps.finalproject.ui.ViewModelFactory
+import com.apps.finalproject.ui.home.HomeViewModel
+import com.apps.finalproject.utils.objectToString
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ArticleListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ArticleListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentArticleListBinding
+    private val homeViewModel: HomeViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
     }
 
     override fun onCreateView(
@@ -35,26 +28,39 @@ class ArticleListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_list, container, false)
+        binding = FragmentArticleListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ArticleListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ArticleListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.getArticle()
+        homeViewModel.article.observe(viewLifecycleOwner){
+            showDataArticle(it)
+        }
+
     }
+
+    private fun showDataArticle(dataArticle: List<Article>) {
+        val listArticleAdapter = ListArticleAdapter(dataArticle)
+        listArticleAdapter.setOnItemClickListener(onItemClick)
+        binding.listArticle.apply {
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = listArticleAdapter
+        }
+    }
+
+    private val onItemClick = object : ListArticleAdapter.OnItemClickListener{
+        override fun onItemClick(article: Article) {
+            detailArticle(article)
+        }
+    }
+
+    private fun detailArticle(article: Article) {
+        val bundle = bundleOf(DetailArticleFragment.EXTRA_ARTICLE to objectToString(article))
+        Navigation.findNavController(requireView()).navigate(R.id.action_articleListFragment_to_detailArticleFragment, bundle)
+    }
+
 }
