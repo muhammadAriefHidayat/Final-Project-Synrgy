65af0bdf318d5b7587d28c58bde222a3ae684839
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-25 01:28:49 +0700

    add detailArticleFragment

--- app/src/main/java/com/apps/finalproject/ui/article/DetailArticleFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/article/DetailArticleFragment.kt
@@ -5,56 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.apps.finalproject.R
+import com.apps.finalproject.databinding.FragmentDetailArticleBinding
+import com.apps.finalproject.remote.model.Article
+import com.apps.finalproject.ui.ViewModelFactory
+import com.apps.finalproject.ui.home.HomeViewModel
+import com.apps.finalproject.utils.Utils
+import com.apps.finalproject.utils.stringToObject
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailArticleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailArticleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentDetailArticleBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val homeViewModel: HomeViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_article, container, false)
+        binding = FragmentDetailArticleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataArticle = arguments?.getString(EXTRA_ARTICLE)
+        val article = stringToObject(dataArticle, Article::class.java)
+        populateDataAtricle(article)
+
+    }
+
+    private fun populateDataAtricle(dataArticle: Article?) {
+        if (dataArticle != null){
+            with(binding){
+                Glide.with(this@DetailArticleFragment)
+                    .load(dataArticle.photo)
+                    .transform(CenterInside(),RoundedCorners(16))
+                    .into(ivBanner)
+                tvTitle.text = dataArticle.title
+                tvWaktuArtikel.text = dataArticle.date
+                tvDesc.text = dataArticle.content
+            }
+        } else {
+            Utils.peringatan(requireContext(), "Koneksi tidak stabil, Terjadi kesalahan")
+            requireActivity().onBackPressed()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailArticleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailArticleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val EXTRA_ARTICLE = "article"
     }
+
 }
