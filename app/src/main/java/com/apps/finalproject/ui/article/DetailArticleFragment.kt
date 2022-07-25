81789e79be53c9799f5295cf90d14e0ccafb11c5
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-07-25 21:58:58 +0700

    implement article detail access to product

--- app/src/main/java/com/apps/finalproject/ui/article/DetailArticleFragment.kt
+++ app/src/main/java/com/apps/finalproject/ui/article/DetailArticleFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
 import com.apps.finalproject.R
 import com.apps.finalproject.databinding.FragmentDetailArticleBinding
@@ -13,8 +16,10 @@ import com.apps.finalproject.remote.model.Article
 import com.apps.finalproject.remote.model.Trending
 import com.apps.finalproject.ui.ViewModelFactory
 import com.apps.finalproject.ui.adapter.ListTrendingAdapter
+import com.apps.finalproject.ui.detail.DetailFragment
 import com.apps.finalproject.ui.home.HomeViewModel
 import com.apps.finalproject.utils.Utils
+import com.apps.finalproject.utils.objectToString
 import com.apps.finalproject.utils.stringToObject
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -49,6 +54,13 @@ class DetailArticleFragment : Fragment() {
         homeViewModel.trending.observe(viewLifecycleOwner){
             showProductTrending(it)
         }
+        homeViewModel.isLoading.observe(viewLifecycleOwner){
+            showLoading(it)
+        }
+
+        binding.tvLihatProdukTrending.setOnClickListener{
+            view.findNavController().navigate(R.id.action_detailArticleFragment_to_productFragment)
+        }
     }
 
     private fun populateDataAtricle(dataArticle: Article?) {
@@ -68,12 +80,28 @@ class DetailArticleFragment : Fragment() {
         }
     }
 
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
     private fun showProductTrending(listProductTrending: List<Trending>){
         val listTrendingAdapter = ListTrendingAdapter(listProductTrending)
+        listTrendingAdapter.setOnItemClickListener(onItemClicked)
         binding.listProdukTrending.apply {
             itemAnimator = DefaultItemAnimator()
             adapter = listTrendingAdapter
         }
+    }
+
+    private val onItemClicked = object : ListTrendingAdapter.OnItemClickListener{
+        override fun onItemClicked(data: Trending) {
+            detailTrending(data)
+        }
+    }
+
+    private fun detailTrending(data: Trending){
+        val mBundle = bundleOf(DetailFragment.EXTRA_PRODUCT to objectToString(data))
+        Navigation.findNavController(requireView()).navigate(R.id.action_detailArticleFragment_to_DetailFragment, mBundle)
     }
 
     companion object {
